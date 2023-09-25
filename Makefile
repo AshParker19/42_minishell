5fca64a323d7e13fdbf60b7e3bfab91ca8e9d9bd
Alexander Stein <astein@student.42lisboa.com>
2023-09-25 19:46:15 +0100

    fixed fata loose from merging

---
+++ Makefile
@@ -0,0 +1,92 @@
+# Makefile for minishell
+# .SILENT:
+
+# Variables
+NAME=minishell
+
+# Prints DEBUG Messages
+DEBUG = 0
+
+# Compiler options
+CC = cc
+CFLAGS = -D DEBUG=$(DEBUG) -Wall -Werror -Wextra -g -fsanitize=address -fsanitize-address-use-after-scope
+CLIBS = -L$(LIB_FOLDER) -lft -lm -lreadline
+CINCLUDES  = -I$(INCLUDE_FOLDER) 
+RM = rm -rf
+
+# ->Colors
+RED    = 	\033[0;31m
+GREEN  = 	\033[0;32m
+ORANGE = 	\033[0;33m
+BLUE   = 	\033[34m
+PURPLE = 	\033[35m
+RESET  = 	\033[0m
+
+# ->Folders
+INCLUDE_FOLDER = ./includes/
+SRC_FOLDER     = ./src/
+OBJ_FOLDER     = ./obj/
+LIB_FOLDER     = ./libft/
+
+# ->Files
+LIBFT = $(LIB_FOLDER)libft.a
+SRCS = $(addprefix $(SRC_FOLDER), 			\
+	0-Core/main.c							\
+	0-Core/env.c							\
+	0-Core/general_utils_1.c				\
+	0-Core/input_manager.c					\
+	0-Core/manage_minibox.c					\
+	0-Core/signals.c						\
+	1-Input-Management/expand_vars.c		\
+	1-Input-Management/lexer.c 				\
+	1-Input-Management/parser.c				\
+	1-Input-Management/utils.c				\
+	2-Execution/builtins/cd.c				\
+	2-Execution/builtins/echo.c				\
+	2-Execution/builtins/pwd.c				\
+	2-Execution/builtins/export.c			\
+	2-Execution/builtins/unset.c			\
+	2-Execution/builtins/env.c				\
+	2-Execution/builtins/exit.c				\
+	)
+
+# Object files
+OBJS = $(SRCS:$(SRC_FOLDER)%.c=$(OBJ_FOLDER)%.o)
+
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++#
+
+# TARGETS
+.PHONY: all clean fclean re norm
+
+all: $(NAME)
+
+$(NAME): $(LIBFT) $(OBJS) 
+	@$(CC) $(OBJS) $(CFLAGS) $(CLIBS) $(CINCLUDES) -o $(NAME)
+	@echo "\n$(GREEN)$(NAME): created$(RESET)"
+
+$(OBJ_FOLDER)%.o: $(SRC_FOLDER)%.c
+	@mkdir -p $(@D)
+	@echo -n "$(BLUE).$(RESET)"
+	@$(CC) $(CFLAGS) $(CINCLUDES)-c $< -o $@
+
+$(LIBFT):
+	@echo "$(ORANGE)compiling: $(LIBFT)\n$(RESET)"
+	@$(MAKE) -sC $(LIB_FOLDER) DEBUG=$(DEBUG)
+
+clean:
+	@$(RM) $(OBJ_FOLDER)
+	@echo "$(BLUE)┌────────────────────┐"
+	@echo "│    $(ORANGE)[✓] CLEANED!$(BLUE)    │"
+	@echo "$(BLUE)└────────────────────┘$(RESET)"
+
+fclean: clean
+	@make -sC $(LIB_FOLDER) fclean
+	@$(RM) $(NAME)
+
+re: fclean all
+
+test: all
+	gnome-terminal --window --hide-menubar --title "ASTEINS MINITALK TESTER" --geometry=60x10+630+100 --working-directory="$(CURDIR)$(TEST_FOLDER)" -- "./run.sh" &
+
+norm:
+	norminette
---
+++ includes/minishell.h
@@ -0,0 +1,89 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: astein <astein@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/07 16:04:05 by astein            #+#    #+#             */
+/*   Updated: 2023/09/25 19:45:47 by astein           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+/* system includes */
+# include <curses.h>
+# include <dirent.h>
+# include <errno.h>
+# include <fcntl.h>
+# include <readline/history.h>
+# include <readline/readline.h>
+# include <signal.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <sys/ioctl.h>
+# include <sys/resource.h>
+# include <sys/stat.h>
+# include <sys/time.h>
+# include <sys/types.h>
+# include <sys/wait.h>
+# include <term.h>
+# include <termios.h>
+# include <unistd.h>
+
+/* project includes */
+# include "../libft/libft_printf.h"
+# include "input_manager.h"
+# include "executor.h"
+
+/*    colors    */
+# define GREEN 		"\033[0;32m"
+# define RED 		"\033[0;31m"
+# define YELLOW 	"\x1b[33m"
+# define CYAN 		"\x1b[36m"
+# define PURPLE 	"\x1b[35m"
+# define RESET 		"\033[0m"
+
+/******************************************************************************/
+/* list of local variables */
+typedef struct s_local_var t_local_var;
+
+typedef struct s_minibox
+{
+    char        **env;
+    char        *input_original;
+    char        *input_trimmed;
+    char        *input_expanded;
+    char        **global_vars;
+    t_token     *tokens;
+    t_tree      *root;
+    t_local_var *local_vars;
+}              t_minibox;
+
+/* list of local variables (definition) */
+typedef struct s_local_var
+{
+    char        *key;
+    char        *value;
+    struct      s_local_var *next;
+}              t_local_var;
+/******************************************************************************/
+
+/* input_manager.c */
+void	manage_input(t_minibox *minibox);
+
+/* env.c */
+char    *resolve_local_var(char *key);
+void    store_local_var(t_minibox *minibox, char *key, char *value);
+void    free_local_vars(t_minibox *minibox);
+
+/* manage_minibox.c */
+void	initialize_box(t_minibox *minibox, char **env);
+void    free_input_strings(t_minibox *minibox);
+void	free_matrix(t_minibox *minibox, int i);
+void    free_and_close_box(t_minibox *minibox, int exit_status);
+
+#endif
