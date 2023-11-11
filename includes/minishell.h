c61438d1e9530848bd00ac863a2bd6288460457f
0815-alex <stein.alexander@posteo.de>
2023-11-11 00:46:30 +0000

    added tester to gitignore and makefile

--- includes/minishell.h
+++ includes/minishell.h
@@ -20,12 +20,12 @@ extern int g_signal_status;
 # define NO_EXIT_STATUS -99999
 
 /* promt strings */
-# define PROMT      "\x1b[36mfrankenshell-->\033[0m "
-# define ERR_PROMT  "\033[38;5;203mfrankenshell:\033[0m "
+// # define PROMT      "\x1b[36mfrankenshell-->\033[0m "
+// # define ERR_PROMT  "\033[38;5;203mfrankenshell:\033[0m "
 
 /* test prompts */
-// # define PROMT "minishell:"
-// # define ERR_PROMT "minishell:"
+# define PROMT "minishell:"
+# define ERR_PROMT "minishell:"
 
 # define FRANKENSHELL_RISES_AMIDTS_DEATH 1
 
