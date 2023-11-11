ea7c9a93eaa3461514f29e46a4d33e565660a073
0815-alex <stein.alexander@posteo.de>
2023-11-11 17:00:41 +0000

    now history write to a correct fd

--- src/3_builtins/history.c
+++ src/3_builtins/history.c
@@ -6,7 +6,7 @@
 /*   By: astein <astein@student.42lisboa.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/06 11:45:50 by anshovah          #+#    #+#             */
-/*   Updated: 2023/11/11 16:47:52 by astein           ###   ########.fr       */
+/*   Updated: 2023/11/11 16:59:50 by astein           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,12 +37,18 @@ void    save_history(t_mbox *mbox, char *inp_hist)
 void    builtin_history(t_mbox *mbox, t_ast *arg_node)
 {
     t_history   *cur;
+	char		*count;
 
     (void)arg_node;
     cur = mbox->history;
     while (cur)
     {
-        printf ("  %d  %s\n", cur->inp_count, cur->inp_hist); //TODO: write to the CMD FD
+		ft_putstr_fd("  ", mbox->executor.io.cmd_fd[CMD_OUT]);
+		count = ft_itoa(cur->inp_count);
+		ft_putstr_fd(count, mbox->executor.io.cmd_fd[CMD_OUT]);
+		free (count);
+		ft_putstr_fd("  ", mbox->executor.io.cmd_fd[CMD_OUT]);
+		ft_putendl_fd(cur->inp_hist, mbox->executor.io.cmd_fd[CMD_OUT]);
         cur = cur->next;
     }
 	set_var_value(mbox, "?", EXIT_STR_SUCCESS);
