9fd926a45fbdf4499d286fd7b692a5de24e57450
0815-alex <stein.alexander@posteo.de>
2023-11-11 03:21:21 +0000

    changed echo! now the wired cases like 'echo -n -nn -nnn Hello' and 'echo -n -nan Wolrd' and 'echo --n lol' and echo -n hi -n wolrld' all work! nice

--- src/3_builtins/echo.c
+++ src/3_builtins/echo.c
@@ -3,14 +3,54 @@
 /*                                                        :::      ::::::::   */
 /*   echo.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: astein <astein@student.42.fr>              +#+  +:+       +#+        */
+/*   By: astein <astein@student.42lisboa.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/13 16:32:13 by astein            #+#    #+#             */
-/*   Updated: 2023/10/29 00:50:41 by astein           ###   ########.fr       */
+/*   Updated: 2023/11/11 03:16:03 by astein           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+/**
+ * @brief	checks if the argument is a flag:
+ * 			the only valid flags is '-n' that tells echo not to print a newline
+ * 			the amount of '-n' flags is irrelevant	e.g. 'echo -n -n -n hello'
+ * 			the amount of 'n' is irrelevant			e.g. 'echno -nn -nnn hello'
+ * 
+ * @param arg 
+ * @param found_text 
+ * @param print_nl 
+ * @return t_bool 
+ */
+static t_bool is_flag(char *arg, t_bool *found_text, t_bool *print_nl)
+{
+	int 	i;
+	t_bool	all_char_n;
+	
+	if (arg[0] != '-')
+		*found_text = ft_true;
+	else
+	{
+		i = 0;
+		all_char_n = ft_false;
+		while (arg[++i] != '\0')
+		{
+			if(arg[i] == 'n')
+				all_char_n = ft_true;
+			else
+			{
+				all_char_n = ft_false;
+				break ;
+			}
+		}
+		if(all_char_n)
+			*print_nl = ft_false;
+		else
+			*found_text = ft_true;
+	}
+	return (!*found_text);
+}
 
 /**
  * @brief   - concatenates all the arg nodes values with single space in between
@@ -18,30 +58,36 @@
  *              - adds a '\n'
  *          - writes to the proper FD
  * 
+ * 			NOTE:
+ * 			'found_text' tells if an printable arg was found before then ignore
+ * 			further flags and print all the args with spaces in between
+ * 			(to deal with those cases)
+ *	 			echo -n hello -n
+ * 				echo -n -n hello
+ * 
  * @param   mbox 
  * @param   arg_node 
  */
 void builtin_echo(t_mbox *mbox, t_ast *arg_node)
 {
-    bool    print_nl;
+    t_bool	print_nl;
+	t_bool	found_text;	
 
-    print_nl = true;
-    if(arg_node)
-    {
-        if (str_cmp_strct(arg_node->content, "-n"))
-        {
-            print_nl = false;
-            arg_node = arg_node->right;
-        }
-        while(arg_node)
-        {
-            ft_putstr_fd(arg_node->content,
+    print_nl = ft_true;
+	found_text = ft_false;
+	while(arg_node)
+	{
+		if(found_text || !is_flag(arg_node->content, &found_text, &print_nl))
+		{
+			ft_putstr_fd(arg_node->content,
                 mbox->executor.io.cmd_fd[CMD_OUT]);
             arg_node = arg_node->right;
             if(arg_node)
                 ft_putchar_fd(' ', mbox->executor.io.cmd_fd[CMD_OUT]);
-        }
-    }
-    if (print_nl)
+		}
+		else
+			arg_node = arg_node->right;
+	}
+	if (print_nl)
         ft_putchar_fd('\n', mbox->executor.io.cmd_fd[CMD_OUT]);    
 }
