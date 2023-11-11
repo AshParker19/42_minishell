ea0ebd40dc6b138bfec2322c85b987aceb6d5ab5
0815-alex <stein.alexander@posteo.de>
2023-11-11 16:30:52 +0000

    added exit status for pwd

--- src/3_builtins/pwd.c
+++ src/3_builtins/pwd.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   pwd.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: anshovah <anshovah@student.42.fr>          +#+  +:+       +#+        */
+/*   By: astein <astein@student.42lisboa.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/13 16:32:55 by astein            #+#    #+#             */
-/*   Updated: 2023/11/07 23:14:09 by anshovah         ###   ########.fr       */
+/*   Updated: 2023/11/11 16:29:33 by astein           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,5 +24,6 @@ void	builtin_pwd(t_mbox *mbox, t_ast *arg_node)
     (void) arg_node;
 
 	ft_putendl_fd(getcwd(NULL, 0), mbox->executor.io.cmd_fd[CMD_OUT]);
+	set_var_value(mbox, "?", EXIT_STR_SUCCESS);
 }
 
