f8ce0403e173ab11b68ac0cd247c81c8b878c960
Anatolii <117525743+AshParker19@users.noreply.github.com>
2023-11-12 10:42:19 +0000

    took some notes of what to change

--- src/3_builtins/header.c
+++ src/3_builtins/header.c
@@ -6,7 +6,7 @@
 /*   By: anshovah <anshovah@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/10 18:00:32 by anshovah          #+#    #+#             */
-/*   Updated: 2023/11/11 16:37:44 by anshovah         ###   ########.fr       */
+/*   Updated: 2023/11/11 17:06:11 by anshovah         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ void	builtin_header(t_mbox *mbox, t_ast *arg_node)
     printf (CYAN"    +:+ +:+         +:+  \n");
     printf (CYAN"   +#+  +:+       +#+     \n");
     printf (CYAN"+#+#+#+#+#+   +#+        \n");
-    printf (CYAN"     #+#    #+#         \n");
+    printf (CYAN"     #+#    #+#         \n");  //FIXME: write to the correct fd
     printf (CYAN"    ###   ########.fr    \n\n");
     printf (YELLOW "astein\t\t" PURPLE " anshovah\n\n"RESET);
     set_var_value(mbox, "?", EXIT_STR_SUCCESS);
