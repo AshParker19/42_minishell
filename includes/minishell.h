7703528580c46af55423c056ca85554037730995
AshParker19 <ashparker4219@gmail.com>
2023-11-17 15:51:48 +0000

    deleted all the system headers that we don't use

--- includes/minishell.h
+++ includes/minishell.h
@@ -18,26 +18,17 @@ extern int g_signal_status;
 
 /* system includes */
 # include <curses.h>
-# include <dirent.h>
 # include <errno.h>
-# include <fcntl.h>
 # include <readline/history.h>
 # include <readline/readline.h>
 # include <signal.h>
 # include <stdio.h>
 # include <stdlib.h>
-# include <string.h>
-# include <sys/ioctl.h>
-# include <sys/resource.h>
 # include <sys/stat.h>
-# include <sys/time.h>
-# include <sys/types.h>
 # include <sys/wait.h>
-# include <term.h>
-# include <termios.h>
 # include <unistd.h>
 
-//TODO: cerror checking FIXME: don't forget to remove at the end
+//TODO: error checking FIXME: don't forget to remove at the end
 // # define malloc(y) NULL
 // # define shift_context_chars(z,c,v) ft_false //TODO:
 // # define fork() -1
