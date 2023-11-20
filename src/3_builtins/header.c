b39ff71d8c31ff35ac06989db82d72fe5b809818
Anatolii <117525743+AshParker19@users.noreply.github.com>
2023-11-20 23:26:40 +0000

    made nice 42 thingy

--- src/3_builtins/header.c
+++ src/3_builtins/header.c
@@ -6,21 +6,36 @@
 /*   By: anshovah <anshovah@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/10 18:00:32 by anshovah          #+#    #+#             */
-/*   Updated: 2023/11/11 17:06:11 by anshovah         ###   ########.fr       */
+/*   Updated: 2023/11/20 23:24:38 by anshovah         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "minishell.h"
 
+# define B_WHITE "\x1b[97m"
+
 void	builtin_header(t_mbox *mbox, t_ast *arg_node)
 {
-    printf (CYAN"\n        :::      ::::::::\n");
-    printf (CYAN"      :+:      :+:    :+:\n");
-    printf (CYAN"    +:+ +:+         +:+  \n");
-    printf (CYAN"   +#+  +:+       +#+     \n");
-    printf (CYAN"+#+#+#+#+#+   +#+        \n");
-    printf (CYAN"     #+#    #+#         \n");  //FIXME: write to the correct fd
-    printf (CYAN"    ###   ########.fr    \n\n");
-    printf (YELLOW "astein\t\t" PURPLE " anshovah\n\n"RESET);
-    set_var_value(mbox, "?", EXIT_STR_SUCCESS);
+	(void)arg_node;
+	ft_putstr_fd(CYAN"       444444444   "YELLOW"222222222222222    \n"\
+	CYAN"      4"B_WHITE"::::::::"CYAN"4  "YELLOW"2"B_WHITE":::::::::::::::"\
+	YELLOW"22  \n"CYAN"     4"B_WHITE":::::::::"CYAN"4  "YELLOW"2"B_WHITE\
+	"::::::"YELLOW"222222"B_WHITE":::::"YELLOW"2 \n"CYAN"    4"B_WHITE"::::"\
+	CYAN"44"B_WHITE"::::"CYAN"4  "YELLOW"2222222     2"B_WHITE":::::"YELLOW"2 \n"
+	CYAN"   4"B_WHITE"::::"CYAN"4 4"B_WHITE"::::"CYAN"4              "YELLOW"2"\
+	B_WHITE":::::"YELLOW"2 \n"CYAN"  4"B_WHITE"::::"CYAN"4  4"B_WHITE"::::"\
+	CYAN"4              "YELLOW"2"B_WHITE":::::"YELLOW"2 \n"CYAN" 4"B_WHITE\
+	"::::"CYAN"4   4"B_WHITE"::::"CYAN"4           "YELLOW"2222"B_WHITE"::::"\
+	YELLOW"2  \n"CYAN"4"B_WHITE"::::"CYAN"444444"B_WHITE"::::"CYAN"444    "\
+	YELLOW"22222"B_WHITE"::::::"YELLOW"22   \n"CYAN"4"B_WHITE"::::::::::::::::"\
+	CYAN"4  "YELLOW"22"B_WHITE"::::::::"YELLOW"222     \n"CYAN"4444444444"\
+	B_WHITE":::::"CYAN"444 "YELLOW"2"B_WHITE":::::"YELLOW"22222        \n"
+	CYAN"          4"B_WHITE"::::"CYAN"4  "YELLOW"2"B_WHITE":::::"YELLOW\
+	"2             \n"CYAN"          4"B_WHITE"::::"CYAN"4  "YELLOW"2"\
+	B_WHITE":::::"YELLOW"2             \n"CYAN"          4"B_WHITE"::::"\
+	CYAN"4  "YELLOW"2"B_WHITE":::::"YELLOW"2       222222\n"CYAN"        44"\
+	B_WHITE"::::::"CYAN"44"YELLOW"2"B_WHITE"::::::"YELLOW"2222222"B_WHITE":::::"\
+	YELLOW"2\n"CYAN"        4"B_WHITE"::::::::"CYAN"4"YELLOW"2"\
+	B_WHITE"::::::::::::::::::"YELLOW"2\n"CYAN"        4444444444"\
+	YELLOW"22222222222222222222\n", mbox->executor.io.cmd_fd[CMD_OUT]);
 }
