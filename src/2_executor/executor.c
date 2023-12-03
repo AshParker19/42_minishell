2dbadb09c8bd2505ec0c73c5b472fcebb6bd0bf4
0815-alex <stein.alexander@posteo.de>
2023-12-03 14:55:27 +0000

    fixed heredoc related waitpid logic. bash spawns all pipe seperated cmds at the same time. we do it as well. but in the case of '<< lol cat | echo hi' the hi should print AFTER terminating the heredoc. in the case 'sleep 1000 | echo hi' the hi should print right away. so before spawninh the next child we quikly check if the child includes a heredoc. if so the parent waits for the child.

--- src/2_executor/executor.c
+++ src/2_executor/executor.c
@@ -6,7 +6,7 @@
 /*   By: astein <astein@student.42lisboa.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/06 18:19:44 by astein            #+#    #+#             */
-/*   Updated: 2023/12/03 11:43:26 by astein           ###   ########.fr       */
+/*   Updated: 2023/12/03 14:52:08 by astein           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,6 @@
 
 static void	exec_child(t_mbox *mbox, t_ast *cmd_node, int cmd_pos, int *cur_p)
 {
-	dprintf(2, "child PID: %d\n", getpid());
 	update_signals(SIGNAL_CHILD);
 	setup_pipes(mbox, cur_p);
 	if (!configure_redir(mbox, cmd_node->left, cur_p))
@@ -34,9 +33,10 @@ static void	exec_child(t_mbox *mbox, t_ast *cmd_node, int cmd_pos, int *cur_p)
 	free_and_close_box_v2(mbox);
 }
 
-static void	exec_parent(t_mbox *mbox, int cmd_pos, int *cur_p)
+static void	exec_parent(t_mbox *mbox, int cmd_pos, int *cur_p, int pid_child, t_ast *cmd_node)
 {
-	dprintf(2, "parent PID: %d\n", getpid());
+	t_ast *cmd_node_cpy;
+	
 	if (cmd_pos == FIRST_CMD || cmd_pos == MIDDLE_CMD)
 		close(cur_p[P_LEFT]);
 	if (cmd_pos != FIRST_CMD && cmd_pos != SINGLE_CMD)
@@ -45,6 +45,16 @@ static void	exec_parent(t_mbox *mbox, int cmd_pos, int *cur_p)
 	mbox->executor.io.prev_pipe[P_RIGHT] = cur_p[P_RIGHT];
 	mbox->executor.io.prev_pipe[P_LEFT] = cur_p[P_LEFT];
 	close_process_fds_v2(mbox);
+	cmd_node_cpy = cmd_node;
+	while (cmd_node_cpy->left)
+	{
+		cmd_node_cpy = cmd_node_cpy->left;
+		if (cmd_node_cpy->type == RED_IN_HD)
+		{
+			waitpid(pid_child, NULL, 0);			
+			break;
+		}
+	}
 }
 
 /**
@@ -58,6 +68,7 @@ static void	exec_parent(t_mbox *mbox, int cmd_pos, int *cur_p)
 static t_bool	execute_cmd(t_mbox *mbox, t_ast *cmd_node, int cmd_pos)
 {
 	int	cur_pipe[2];
+	int child_pid;
 
 	initialize_io(mbox);
 	if (cmd_pos == SINGLE_CMD && is_cmd_builtin(mbox, cmd_node->content))
@@ -69,13 +80,14 @@ static t_bool	execute_cmd(t_mbox *mbox, t_ast *cmd_node, int cmd_pos)
 			if (pipe(cur_pipe) < 0)
 				return (err_free_and_close_box(mbox, EXIT_FAILURE));
 		mbox->executor.pid[mbox->executor.pid_index] = fork();
-		if (mbox->executor.pid[mbox->executor.pid_index] < 0)
+		child_pid = mbox->executor.pid[mbox->executor.pid_index];
+		if (child_pid < 0)
 			return (err_free_and_close_box(mbox, EXIT_FAILURE));
 		update_signals(SIGNAL_PARENT);
-		if (mbox->executor.pid[mbox->executor.pid_index] == 0)
+		if (child_pid == 0)
 			exec_child(mbox, cmd_node, cmd_pos, cur_pipe);
 		else
-			exec_parent(mbox, cmd_pos, cur_pipe);
+			exec_parent(mbox, cmd_pos, cur_pipe, child_pid, cmd_node);
 	}
 	return (ft_true);
 }
