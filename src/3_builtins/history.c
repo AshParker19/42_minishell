9352696a821e142c509c7560d4e922fb6fef3639
AshParker19 <ashparker4219@gmail.com>
2023-12-05 10:12:35 +0000

    checked history on schools PC and fixed it

--- src/3_builtins/history.c
+++ src/3_builtins/history.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   history.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: astein <astein@student.42lisboa.com>       +#+  +:+       +#+        */
+/*   By: anshovah <anshovah@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/06 11:45:50 by anshovah          #+#    #+#             */
-/*   Updated: 2023/12/05 00:50:14 by astein           ###   ########.fr       */
+/*   Updated: 2023/12/05 10:09:08 by anshovah         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -67,7 +67,7 @@ void	save_history(t_mbox *mbox)
 	new_node = ft_calloc(1, sizeof(t_history));
 	if (!new_node)
 		return ;
-	new_node->inp = ft_strdup(mbox->inp_trim);
+	new_node->inp = ft_strdup(mbox->inp_orig);
 	new_node->index = ++i;
 	new_node->mbox = mbox;
 	ft_lstadd_back(&(mbox->history_lst), ft_lstnew(new_node));
